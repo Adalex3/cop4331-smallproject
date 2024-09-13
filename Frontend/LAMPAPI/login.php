a25a0b0d5f581da16b49e63f3b391d6958a9216b
Adalex3 <adalex3@icloud.com>
2024-09-13 13:47:12 -0400

    testing php

--- Frontend/LAMPAPI/login.php
+++ Frontend/LAMPAPI/login.php
@@ -1,54 +1,56 @@
 <?php
 
-$data = getRequestInfo();
+echo json_encode(["message" => "TEST TEST TEST!!"]);
 
-$conn = new mysqli("localhost", "root", "qUJ@lHgJrNi1", "contactManager");
+// $data = getRequestInfo();
 
-if($conn->connect_error)
-{
-    die("Connetion failed: " . $conn->connect_error);
-}
+// $conn = new mysqli("localhost", "root", "qUJ@lHgJrNi1", "contactManager");
 
-if(isset($data->username) && isset($data->Password))
-{
-    $username = htmlspecialchars($data->username);
-    $password = htmlspecialchars($data->Password);
+// if($conn->connect_error)
+// {
+//     die("Connetion failed: " . $conn->connect_error);
+// }
 
-    $stmt = $conn->prepare("SELECT Password FROM Users WHERE username = ?");
-    $stmt->bind_param("s", username);
-    $stmt->execute();
-    $stmt->store_result();
+// if(isset($data->username) && isset($data->Password))
+// {
+//     $username = htmlspecialchars($data->username);
+//     $password = htmlspecialchars($data->Password);
 
-    if(stmt->num_rows > 0) 
-    {
-        $stmt->bind_result($hashedPassword);
-        $stmt->fetch();
+//     $stmt = $conn->prepare("SELECT Password FROM Users WHERE username = ?");
+//     $stmt->bind_param("s", username);
+//     $stmt->execute();
+//     $stmt->store_result();
 
-        if(password_verify($password, $hashedPassword))
-        {
-            echo json_encode(["message" => "Login successful."]);
-        }
-        else
-        {
-            echo json_encode(["error" => "Incorrect password."]);
-        }
-    }
-    else 
-    {
-        echo json_encode(["error" => "Username not found."]);
-    }
-    $stmt->close();
-}
+//     if(stmt->num_rows > 0) 
+//     {
+//         $stmt->bind_result($hashedPassword);
+//         $stmt->fetch();
 
-else 
-{
-    echo json_encode(["error" => "Invalid input"]);
-}
+//         if(password_verify($password, $hashedPassword))
+//         {
+//             echo json_encode(["message" => "Login successful."]);
+//         }
+//         else
+//         {
+//             echo json_encode(["error" => "Incorrect password."]);
+//         }
+//     }
+//     else 
+//     {
+//         echo json_encode(["error" => "Username not found."]);
+//     }
+//     $stmt->close();
+// }
 
-$conn->close();
+// else 
+// {
+//     echo json_encode(["error" => "Invalid input"]);
+// }
 
-function getRequestInfo() 
-{
-    return json_decode(file_get_contents('php://input'), true);
-}
+// $conn->close();
+
+// function getRequestInfo() 
+// {
+//     return json_decode(file_get_contents('php://input'), true);
+// }
 ?>
