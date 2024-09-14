551f79f9cb78d6af38a39d8be06867638f6f3d7f
Kush Havinal <kush.havinal@gmail.com>
2024-09-14 17:06:55 -0400

    trying login

--- Backend/Login.php
+++ Backend/Login.php
@@ -6,7 +6,7 @@
 	$firstName = "";
 	$lastName = "";
 
-    $conn = new mysqli("localhost", "root", "qUJ@lHgJrNi1", "contactManager");
+    $conn = new mysqli("172.26.4.41", "root", "qUJ@lHgJrNi1", "contactManager");
 
 
     if ($conn->connect_error) {
