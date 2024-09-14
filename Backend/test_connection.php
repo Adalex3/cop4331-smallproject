4df3cd8a3750427f0dd8ed997222f051aa340714
Kush Havinal <kush.havinal@gmail.com>
2024-09-14 16:19:08 -0400

    testing sql connection

---
+++ Backend/test_connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost"; // or the IP address of your MySQL server
+$username = "TheBeast";        // MySQL root user
+$password = "qUJ@lHgJrNi1"; // replace with the actual root password
+$database = "contactManager";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
