096f77dd545b5bf2dbdca475c4dc33111db11a2c
afetyko <afetyko03@classvaluation.com>
2024-09-17 20:57:06 -0400

    change

--- Backend/Login.php
+++ Backend/Login.php
@@ -1,66 +1,74 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    
-    $inData = getRequestInfo();
-    
-    $id = 0;
-    $firstName = "";
-    $lastName = "";
 
-    $conn = new mysqli("127.0.0.1", "badridemo", "badridemo1", "contactManager");
+$inData = getRequestInfo();
 
-    if ($conn->connect_error) {
-        returnWithError($conn->connect_error);
-    } else {
-        // Prepare and execute the SQL statement to get the user
-        $stmt = $conn->prepare("SELECT ID, FirstName, LastName, Password FROM Users WHERE Login=?");
+$id = 0;
+$firstName = "";
+$lastName = "";
+
+$conn = new mysqli("127.0.0.1", "badridemo", "badridemo1", "contactManager");
+
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    // Prepare SQL statement to fetch user data
+    $stmt = $conn->prepare("SELECT ID, FirstName, LastName, Password FROM Users WHERE Login=?");
+    if ($stmt) {
+        // Bind parameters and execute
         $stmt->bind_param("s", $inData["login"]);
         $stmt->execute();
         $result = $stmt->get_result();
-
+        
         if ($row = $result->fetch_assoc()) {
-            // Verify the password
-            if (password_verify($inData["password"], $row['Password'])) {
+            // Verify password
+            if (password_verify($inData["password"], $row["Password"])) {
+                // Return user info if password matches
                 returnWithInfo($row['FirstName'], $row['LastName'], $row['ID']);
             } else {
-                returnWithError("Invalid login credentials");
+                returnWithError("Invalid credentials");
             }
         } else {
             returnWithError("No Records Found");
         }
-
+        
         $stmt->close();
-        $conn->close();
+    } else {
+        returnWithError("Failed to prepare statement");
     }
 
-    function getRequestInfo() {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+    $conn->close();
+}
 
-    function sendResultInfoAsJson($obj) {
-        header('Content-type: application/json');
-        echo $obj;
-    }
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-    function returnWithError($err) {
-        $retValue = json_encode(array(
-            "id" => 0,
-            "firstName" => "",
-            "lastName" => "",
-            "error" => $err
-        ));
-        sendResultInfoAsJson($retValue);
-    }
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
 
-    function returnWithInfo($firstName, $lastName, $id) {
-        $retValue = json_encode(array(
-            "id" => $id,
-            "firstName" => $firstName,
-            "lastName" => $lastName,
-            "error" => ""
-        ));
-        sendResultInfoAsJson($retValue);
-    }
+function returnWithError($err)
+{
+    $retValue = json_encode(array(
+        "id" => 0,
+        "firstName" => "",
+        "lastName" => "",
+        "error" => $err
+    ));
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($firstName, $lastName, $id)
+{
+    $retValue = json_encode(array(
+        "id" => $id,
+        "firstName" => $firstName,
+        "lastName" => $lastName,
+        "error" => ""
+    ));
+    sendResultInfoAsJson($retValue);
+}
 ?>
