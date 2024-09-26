5d54c68a023b16731bca263edc7e23b62cfe1d1e
KushH305 <101956778+KushH305@users.noreply.github.com>
2024-09-25 23:13:10 -0400

    testing diff user

--- Frontend/js/code.js
+++ Frontend/js/code.js
@@ -228,7 +228,7 @@ function fetchContacts() {
     }
 
     // Hardcoding the username "afetyko"
-    let payload = JSON.stringify({ username: "afetyko" });
+    let payload = JSON.stringify({ username: "fdggfdsf" });
     xhr.send(payload);
 }
 
